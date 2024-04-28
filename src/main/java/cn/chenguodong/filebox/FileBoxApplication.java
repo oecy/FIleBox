2cfd04e72a25d18330c2711739fe1871a07f2529
oecy <cgdstudy@qq.com>
2024-04-28 09:15:42 +0800

    first commit

---
+++ src/main/java/cn/chenguodong/filebox/FileBoxApplication.java
@@ -0,0 +1,13 @@
+package cn.chenguodong.filebox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileBoxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileBoxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/chenguodong/filebox/FileBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.chenguodong.filebox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileBoxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
